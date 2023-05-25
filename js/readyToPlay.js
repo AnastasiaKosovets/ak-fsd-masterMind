cd44edb874b6c98ebab1d297735ea7d222acb0af
Anastasia <anastasiakosovets@gmail.com>
2023-05-25 20:08:28 +0200

    feat: first try with math.random for secret anwer, it works, but now I need to paint my circles with it

--- js/readyToPlay.js
+++ js/readyToPlay.js
@@ -49,29 +49,43 @@ let dificultad = sessionStorage.getItem("forLevel");
     }
   }
 
-//   pruebas para guardar los colores seleccionados y pasarlos a mis divs
+  //   pruebas para guardar los colores seleccionados y pasarlos a mis divs
 
 // Este es el contenedor donde se van a agregar los colores en el HTML.
-const myColorsForPlayElement = document.querySelector("#myColorsForPlay > div");
+let myColorsForPlayElement = document.querySelector("#myColorsForPlay > div");
+let colorsFromLocalStorage = JSON.parse(sessionStorage.getItem("myColor"));
 
-// Recogemos el valor que anteriormente guardamos en el sessionStorage.
-const colorsFromLocalStorage = JSON.parse(sessionStorage.getItem("myColor"));
-console.log("Estos son los colores que devuelve el sessionStorage:", colorsFromLocalStorage);
+//Aquí creamos un array de todos los valores de ese objeto
 
-//Aquí creamos un array de todos los valores de ese objeto, usamos lo siguiente.
-const colors = Object.values(colorsFromLocalStorage);
-console.log(colors)
+let colors = Object.values(colorsFromLocalStorage);
 
-// Ya que tenemos los colores disponibles los crearmos y los agregamso al HTML.
+// con los colores disponibles creamos el hueco con su html correspondiente
 
 colors.map((color, index) => {
-  const selectedColor = document.createElement("div");
-  selectedColor.classList.add("circleC-myColor");
-  selectedColor.id = "selectedColorsOfPicker" + index;
-  selectedColor.style.backgroundColor = color;
-  myColorsForPlayElement.appendChild(selectedColor);
-})
+    let selectedColor = document.createElement("div");
+    selectedColor.classList.add("circleC-myColor");
+    selectedColor.id = "selectedColorsOfPicker" + index;
+    selectedColor.style.backgroundColor = color;
+    myColorsForPlayElement.appendChild(selectedColor);
+});
 
+
+
+// pruebas combinacion secreta
+
+let randomColors = [];
+
+const secretAnswerColor = () => {
+
+    let transformToArrayColors = Object.values(colorsFromLocalStorage);
+
+    for( let i = 0; i < transformToArrayColors.length; i++){
+        let randomMethod = Math.floor(Math.random() * (transformToArrayColors.length));
+        randomColors.push(transformToArrayColors[randomMethod]);
+    }
+}
+secretAnswerColor();
+console.log(randomColors);
 
 
 
