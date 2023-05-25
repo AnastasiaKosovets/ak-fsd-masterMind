b5960755f4050d8183dc6357f7fc74970abb5675
Anastasia <anastasiakosovets@gmail.com>
2023-05-25 13:46:34 +0200

    feat: first try of save choosen colors and put them in next page of the game

--- js/readyToPlay.js
+++ js/readyToPlay.js
@@ -49,6 +49,29 @@ let dificultad = sessionStorage.getItem("forLevel");
     }
   }
 
+//   pruebas para guardar los colores seleccionados y pasarlos a mis divs
+
+// Este es el contenedor donde se van a agregar los colores en el HTML.
+const myColorsForPlayElement = document.querySelector("#myColorsForPlay > div");
+
+// Recogemos el valor que anteriormente guardamos en el sessionStorage.
+const colorsFromLocalStorage = JSON.parse(sessionStorage.getItem("myColor"));
+console.log("Estos son los colores que devuelve el sessionStorage:", colorsFromLocalStorage);
+
+//Aquí creamos un array de todos los valores de ese objeto, usamos lo siguiente.
+const colors = Object.values(colorsFromLocalStorage);
+console.log(colors)
+
+// Ya que tenemos los colores disponibles los crearmos y los agregamso al HTML.
+
+colors.map((color, index) => {
+  const selectedColor = document.createElement("div");
+  selectedColor.classList.add("circleC-myColor");
+  selectedColor.id = "selectedColorsOfPicker" + index;
+  selectedColor.style.backgroundColor = color;
+  myColorsForPlayElement.appendChild(selectedColor);
+})
+
 
 
 
