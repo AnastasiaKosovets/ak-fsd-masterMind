e0dcdfe1ab976154938f2b668407b50d90c8708d
Anastasia <anastasiakosovets@gmail.com>
2023-05-27 14:23:26 +0200

    fix: change creation of board fills, rows etc, because needed to work with each fill.

--- js/readyToPlay.js
+++ js/readyToPlay.js
@@ -2,48 +2,46 @@
 
 let containerR = document.getElementById("containerR");
 
-const createMyFills = () => {
+const createMyFills = (numRows) => {
 
-    let fila = document.createElement("div");
-    fila.className = "fila";
-
-    for (let i = 0; i < 4; i++) {
-
-        let squareR = document.createElement("div");
-        squareR.className = "squareR";
-        fila.appendChild(squareR);
-    }
-
-    let grid = document.createElement("div");
-    grid.className = "grid";
-    fila.appendChild(grid);
-
-    for (let i = 0; i < 4; i++) {
-
-        let gridItem = document.createElement("div");
-        gridItem.className = "grid-item";
-        grid.appendChild(gridItem)
-
-    }
-    containerR.appendChild(fila);
-};
+    for (let i = 0; i < numRows; i++) {
+     let fila = document.createElement("div");
+     fila.className = "fila";
+ 
+     for (let x = 0; x < 4; x++) {
+ 
+         let squareR = document.createElement("div");
+         squareR.className = "squareR";
+         fila.appendChild(squareR);
+     }
+ 
+     let grid = document.createElement("div");
+     grid.className = "grid";
+     fila.appendChild(grid);
+ 
+     for (let x = 0; x < 4; x++) {
+ 
+         let gridItem = document.createElement("div");
+         gridItem.className = "grid-item";
+         grid.appendChild(gridItem)
+ 
+     }
+ 
+     containerR.appendChild(fila);
+ 
+    }  
+ };
 
 //  Según el nivel te hace el tablero correspondiente
 
 let dificultad = sessionStorage.getItem("forLevel");
 
 if (dificultad === "beginnerRow") {
-    for (let i = 0; i < 10; i++) {
-        createMyFills();
-    }
+    createMyFills(10);
 } else if (dificultad === "mediumRow") {
-    for (let i = 0; i < 8; i++) {
-        createMyFills();
-    }
+    createMyFills(8);
 } else if (dificultad === "advancedRow") {
-    for (let i = 0; i < 6; i++) {
-        createMyFills();
-    }
+    createMyFills(6);
 }
 
 //  Guardamos los colores seleccionados y pasamos a los divs
@@ -72,8 +70,6 @@ const firstRowSquares = document.querySelectorAll(".fila:first-child .squareR")
             let nextColorIndex = (currentColorIndex + 1) % colors.length;
             let nextColor = colors[nextColorIndex];
             square.style.backgroundColor = nextColor;
-    
-            
         });
     })
     
@@ -102,7 +98,7 @@ const secretAnswerColor = () => {
         randomColors.push(transformToArrayColors[randomColorIndex]);
         indexArray.splice(randomIndex, 1);
     }
-}
+};
 secretAnswerColor();
 
 // Pintamos con colores aleatorios los circulos
@@ -138,36 +134,34 @@ const checkMyAnswer = () => {
 
     //  aqui declaro que: usando la comparación anterior me pinte los circulos pequeños de color rojo
     // si coincide la posicion y de color blanco si coincide el color en sí, utilizando el metodo includes dentro de un if..else if..else
-    gridItems.forEach((gridItem, index) => {
-        if(matchedPosicions.includes(index)){
-            gridItem.style.backgroundColor = "red";
-        } else if(matchedColors.includes(index)) {
-            gridItem.style.backgroundColor = "white";
-        } else {
-            gridItem.style.backgroundColor = "transparent";
-        }
-    });
+        gridItems.forEach((gridItem, index) => {
+            if(index < 4) {
+                if (matchedPosicions.includes(index)) {
+                    gridItem.style.backgroundColor = "red";
+                } else if (matchedColors.includes(index)) {
+                    gridItem.style.backgroundColor = "white";
+                } else {
+                    gridItems.style.backgroundColor = "transparent";
+                }
+            }
+        });
+    // gridItems.forEach((gridItem, index) => {
+    //     if(matchedPosicions.includes(index)){
+    //         gridItem.style.backgroundColor = "red";
+    //     } else if(matchedColors.includes(index)) {
+    //         gridItem.style.backgroundColor = "white";
+    //     } else {
+    //         gridItem.style.backgroundColor = "transparent";
+    //     }
+    // });
 
     let correctAnswerSecretChoise = Array.from(firstRowSquares, (el) => el.style.backgroundColor);
     
-            if (JSON.stringify(correctAnswerSecretChoise) === JSON.stringify(randomColors)) {
-                // console.log("has acertado con los colores");
-                window.location.href = "./winner.html"
-            } else if (JSON.stringify(correctAnswerSecretChoise) != JSON.stringify(randomColors)) {
-                
-            }
-    // console.log("colores que aparecen en array:", matchedColors);
-    // console.log("posiciones que han coincidido:", matchedPosicions);
-
-
-    // for (let i = 0; i < randomColors.length; i++) {
-    //     if (yourCombination[i] === randomColors[i]) {
-    //         console.log("has acertado");
-    //     } else {
-    //         console.log("has perdido");
-    //     }
-    // }
+        if (JSON.stringify(correctAnswerSecretChoise) === JSON.stringify(randomColors)) {
+            // console.log("has acertado con los colores");
+            window.location.href = "./winner.html"
+        } else if (JSON.stringify(correctAnswerSecretChoise) != JSON.stringify(randomColors)) {
+            
+        }
 };
 checkMyAnswer();
-
-
